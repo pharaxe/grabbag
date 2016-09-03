02b95d862629fbcce80d1b5017b3bf733ae5edb6
Benjamin Sweedler <bsweedler@gmail.com>
2016-09-02 21:27:00 -0500

    api call works in php

--- bag_script.js
+++ bag_script.js
@@ -1,13 +1,8 @@
-include mootools-core
-
 var requestCategories = new Request({
-   url: 'https://www.ifixit.com/api/2.0/categories'
-   onProgress: function(event, xhr){
-      var loaded = event.loaded, total = event.total;
-
-      console.log(parseInt(loaded / total * 100, 10));
+   url: 'https://www.ifixit.com/api/2.0/categories',
+   onSuccess: function(responseText) {
+      console.log(responseText);
    }
 });
 
-var devices = requestCategories.send();
-console.log(devices);
+var devices = requestCategories.put();
