d8c3b50fd84c1d707f23e07efb2e945ca9648dee
Benjamin Sweedler <bsweedler@gmail.com>
2016-09-16 23:31:28 -0500

    js fetches the hierarchy of categories and displays it in a list.

--- bag_script.js
+++ bag_script.js
@@ -1,8 +1,25 @@
-var requestCategories = new Request({
+var requestCategories = new Request.JSON({
    url: 'https://www.ifixit.com/api/2.0/categories',
-   onSuccess: function(responseText) {
-      console.log(responseText);
+   onSuccess: function(responseJSON, responseText) {
+      addDevicesRecursively($('devices'), responseJSON);
    }
 });
 
-var devices = requestCategories.put();
+requestCategories.get();
+
+function addDevicesRecursively(parentElement, hierarchy) {
+   if (typeof hierarchy === 'string') {
+      parentElement.grab(new Element('li', {
+         html: hierarchy
+         }));
+   } else if (typeof hierarchy === 'object' && hierarchy) {
+      var deviceList = new Element('ul');
+      Object.each(hierarchy, function(value, key) {
+         category = new Element('li', {
+            html: key});
+         deviceList.grab(category);
+         addDevicesRecursively(category, value);
+         });
+      parentElement.grab(deviceList);
+   }
+}
